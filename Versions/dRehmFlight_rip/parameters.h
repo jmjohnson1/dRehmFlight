09fb994e06c756ca16ffc557aa11684cdd8870dc
jmjohnson1 <106408381+jmjohnson1@users.noreply.github.com>
2023-09-04 11:18:19 -0500

    updated

--- Versions/dRehmFlight_rip/parameters.h
+++ Versions/dRehmFlight_rip/parameters.h
@@ -1,9 +1,33 @@
 #ifndef PARAM_H
 #define PARAM_H
 
-class Parameter {
+// YOU MUST CHANGE THIS WHEN ADDING OR REMOVING PARAMETERS
+// I do this because "new" scares me in embedded programs.
+// Maybe this can change later.
+#define NUMBER_OF_PARAMS 3
+
+#include <stdint.h>
+
+typedef struct param_s {
+	char name[16];
+	float value;
+	uint8_t mavType;
+
+	param_s(char parameterName[16], float parameterValue, uint8_t parameterType) {
+		for (int i = 1; i < 15; ++i) {
+			name[i] = parameterName[i];
+		}
+		value = parameterValue;
+		mavType = parameterType;
+	}
+} param_t;
+
+class ParameterTracker {
 public:
-parameter
-}
+	ParameterTracker();
+
+private:
+	param_t params[NUMBER_OF_PARAMS];
+};
 
 #endif //PARAM_H
